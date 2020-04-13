1da88523c6ab28a26ca913414f7ea3b4814fa0a4
Yazek13 <fedorov_d_l@mail.ru>
2020-04-13 22:42:48 +1000

    Тренируемся писать игры 2

---
+++ Drakones.py
@@ -0,0 +1,44 @@
+
+import random
+import time
+
+def displayIntro():
+
+    print('''Вы находитесь в землях, заселенных драконами.
+6. Перед собой вы видите две пещеры. В одной из них — дружелюбный дракон,
+7. который готов поделиться с вами своими сокровищами. Во второй —
+8. жадный и голодный дракон, который мигом вас съест.''')
+    print()
+
+def chooseCave():
+    cave = ''
+    while cave != '1' and cave != '2':
+        print('В какую пещеру вы войдете? (нажмите клавишу 1 или 2)')
+        cave = input()
+    return cave
+
+def checkCave(chosenCave):
+    print('Вы приближаетесь к пещере...')
+    time.sleep(2)
+    print('Ее темнота заставляет вас дрожать от страха...')
+    time.sleep(2)
+    print('Большой дракон выпрыгивает перед вами! Он раскрывает свою пасть и...')
+    print()
+    time.sleep(2)
+
+    friendlyCave = random.randint(1, 2)
+
+    if chosenCave == str(friendlyCave):
+        print('...делится с вами своими сокровищами!')
+    else:
+        print('...моментально вас съедает!')
+
+playAgain = 'да'
+
+while playAgain == 'да' or playAgain == 'д':
+    displayIntro()
+    caveNumber = chooseCave()
+    checkCave(caveNumber)
+
+    print('Попытаете удачу еще раз? (да или нет)')
+    playAgain = input()
